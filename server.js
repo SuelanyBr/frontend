1df310a676fbdfbdc72fee10155d2c2c97ad8411
Suelany Brito <suelanybr@gmail.com>
2019-06-04 02:51:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'./dist/src'))
+app.use(express.static('./dist/opi-frontEnd'))
 
 app.listen(process.env.PORT || 3000)
 
-app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'./dist/src/index.html'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'./dist/opi-frontEnd/index.html'));
 });
